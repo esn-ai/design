b72fc622b68e60563711fd28619a8ddb775d377b
Oleksiy Pastukhov <oleksiy.pastukhov@senseteq.io>
2020-12-23 00:42:36 +0200

    Updates

---
+++ precache-manifest.b50245205d8898aaafcc37f376a90b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675f4083fd53db7ff70f0bb78497ca85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a13e6cee14c7cb432f39",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "d3c36c59be1c2a56411f",
+    "url": "./static/js/2.4bdee88a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4bdee88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13e6cee14c7cb432f39",
+    "url": "./static/js/main.a02868d8.chunk.js"
+  },
+  {
+    "revision": "637871e2b510a3b5fbbd",
+    "url": "./static/js/runtime-main.8636638a.js"
+  }
+]);
